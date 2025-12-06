bd560818171be77438401a87d275ba403916eecd
Максим Блинов <126991331+maskon@users.noreply.github.com>
2025-12-06 17:29:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
 const calculatorElem = document.getElementById('calculator')
-
 const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
 const input3 = document.getElementById('input3')
 const input4 = document.getElementById('input4')
-
+const input5 = document.getElementById('input5')
 const submitBtn = document.getElementById('submit')
 const cleanInput = document.getElementById('clean')
-
 const blockResult = document.getElementById('block--result')
-const resultElement = document.getElementById('result')
-const resultElementClean = document.getElementById('result-clean')
-const resultElementNal = document.getElementById('result-nal')
-const resultElementSubtitle = document.getElementById('result-subtitle')
-const resultEl = document.querySelectorAll('.resultat')
 
 let sum
 let sumClean
 let sumNal
+let sumNight
 
 calculatorElem.addEventListener('input', (e) => {     
-    if (e.target.dataset.type === "input1") {examinationInput(e)}    
-    if (e.target.dataset.type === "input2") {examinationInput(e)}  
-    if (e.target.dataset.type === "input3") {examinationInput(e)}   
-    if (e.target.dataset.type === "input4") {examinationInput(e)}
+    if (e.target.dataset.type === "input1" || 
+        e.target.dataset.type === "input2" || 
+        e.target.dataset.type === "input3" || 
+        e.target.dataset.type === "input4" ||
+        e.target.dataset.type === "input5") 
+    {examinationInput(e)}
 })
 
 function examinationInput(e) { return Number(e.target.value) < 0 ? e.target.value = 0 : e.target.value }
 
-submitBtn.onclick = function() {
-    
+submitBtn.onclick = function() {   
     blockResult.style.display = 'block'
 
-    const sum1 = Number(input1.value) * 8
-    const sum2 = Number(input2.value) - Number(input3.value)
-    const sum3 = sum1 * sum2
-    const sum4 = sum1 / 100 * 40
-    const sum5 = sum4 * Number(input3.value)
-    const sum6 = sum1 * Number(input3.value)
-    const sum7 = sum5 + sum6
-    const sum8 = sum2 + Number(input3.value)
-    const sum9 = sum8 * 40
-    const sum10 = Number(input4.value) * sum1 //sum1 / 2 * Number(input4.value)
+    const rate = Number(input1.value) * 8
+    const dayShifts = Number(input2.value) - Number(input3.value)
+    const moneyDays = rate * dayShifts
+    const rateNight = rate / 100 * 40
+    const moneyRateNight = rateNight * Number(input3.value)
+    const moneyNight = rate * Number(input3.value)
+    const monye = moneyRateNight + moneyNight
+    const allShifts = dayShifts + Number(input3.value)
+    const moneyMilk = allShifts * 40
+    const monyeHolliday = Number(input4.value) * rate
+    const moneyWeekend = rate / 2 * Number(input5.value)
     
-    sum = sum3 + sum7 + sum9 + sum10
+    sum = moneyDays + monye + moneyMilk + monyeHolliday + moneyWeekend
 
     const sumPercent = sum / 100 * 13
     
     sumClean = sum - sumPercent
     sumNal = sum - sumClean
+    sumNight = moneyRateNight
+    sumMilk = moneyMilk
     
     if (input1.value === '' || input2.value === '') {
         renderBlock()
@@ -71,7 +69,7 @@ submitBtn.onclick = function() {
         input3.style.border = '2px solid red'
         input4.style.border = '2px solid red'
         input3.focus()
-        resultElementSubtitle.innerHTML = 'Ошибка! Ночных и праздничных смен не может быть больше!'
+        blockResult.innerHTML = 'Ночных и праздничных смен не может быть больше!'
     }
     
     else if (Number(input3.value) > Number(input2.value)) {
@@ -79,7 +77,7 @@ submitBtn.onclick = function() {
         renderEror()
         input3.style.border = '2px solid red'
         input3.focus()
-        resultElementSubtitle.innerHTML = 'Ошибка! Ночных смен не может быть больше!'
+        blockResult.innerHTML = 'Ночных смен не может быть больше!'
     }
     
     else if (Number(input4.value) > Number(input2.value)) {
@@ -87,73 +85,63 @@ submitBtn.onclick = function() {
         renderEror()
         input4.style.border = '2px solid red'
         input4.focus()
-        resultElementSubtitle.innerHTML = 'Ошибка! Праздничных смен не может быть больше!'
+        blockResult.innerHTML = 'Праздничных смен не может быть больше!'
+    }
+
+    else if (Number(input5.value) > Number(input2.value)) {
+        renderBlock()
+        renderEror()
+        input5.style.border = '2px solid red'
+        input5.focus()
+        blockResult.innerHTML = 'Выходных смен не может быть больше!'
     }
     
     else { renderBlock() } 
 }
 
-cleanInput.onclick = function() {
-    
+cleanInput.onclick = function() {    
     blockResult.style.display = 'none'
-    
     input1.value = ''
     input2.value = ''
     input3.value = ''
     input4.value = ''
-    
-    resultElement.innerHTML = ''
-    
-    resultElementClean.innerHTML = ''
-    
-    resultElementNal.innerHTML = ''
+    input5.value = ''
     
     renderInput()
 }
 
 function renderBlock() {
+    blockResult.style.color = 'black'
     
-    resultElementSubtitle.innerHTML = 'Размер з/п за вычетом НДФЛ:'
-    
-    resultElementCleanHTML = sumClean.toLocaleString('ru-RU', {
-        minimumFractionDigits: 0,      
-        maximumFractionDigits: 2,
-    })
-    
-    resultElementHTML = sum.toLocaleString('ru-RU', {
-        minimumFractionDigits: 0,      
-        maximumFractionDigits: 2,
-    })
-    
-    resultElementNalHTML = sumNal.toLocaleString('ru-RU', {
-        minimumFractionDigits: 0,      
-        maximumFractionDigits: 2,
-    })
-    
-    resultEl.forEach( function (item) {
-        item.style.color = '#333333'
-        item.style.fontWeight = '700'
-    })
-    
-    resultElementSubtitle.style.color = '#333333'
-    
-    resultEl[0].innerHTML = `<span class="result-sp result-sp-fs" style="color: #1668e3;">${resultElementCleanHTML} ₽</span>`
-    
-    resultEl[1].innerHTML = `Начисленная сумма зарплаты: <span class="result-sp">${resultElementHTML} ₽</span>` 
-    
-    resultEl[2].innerHTML = `Сумма налога: <span class="result-sp">${resultElementNalHTML} ₽</span>`
+    blockResult.innerHTML = `
+        <div class="mb">
+            <p class="subtitle">Размер зарплаты:</p>
+            <p class="result-sp result-sp-fs" style="color: #1668e3;">${sumClean.toLocaleString('ru-RU', { minimumFractionDigits: 0, maximumFractionDigits: 2 })} ₽</p>
+        </div>
+        <div class="fl">
+            <img src="img/earnings.svg" alt="earnings" width="20px"> Начисленная сумма: 
+            <span class="result-sp">${sum.toLocaleString('ru-RU', { minimumFractionDigits: 0, maximumFractionDigits: 2 })} ₽</span>
+        </div>
+        <div class="fl">
+            <img src="img/weather-few-clouds-night.svg" alt="night" width="20px"> Ночные: 
+            <span class="result-sp">${sumNight.toLocaleString('ru-RU', { minimumFractionDigits: 0, maximumFractionDigits: 2 })} ₽</span>
+        </div>
+        <div class="fl">
+            <img src="img/milk-carton.svg" alt="milk" width="20px"> Компенсация молока: 
+            <span class="result-sp">${sumMilk.toLocaleString('ru-RU', { minimumFractionDigits: 0, maximumFractionDigits: 2 })} ₽</span>
+        </div>
+        <div class="fl">
+            <img src="img/tax.svg" alt="nalog" width="20px"> Сумма налога: 
+            <span class="result-sp">${sumNal.toLocaleString('ru-RU', { minimumFractionDigits: 0, maximumFractionDigits: 2 })} ₽</span>
+        </div>`
    
     renderInput() 
 }
 
-function renderEror() {
-    
-    resultEl.forEach( function (item) {
-        item.innerHTML = ''
-    })
-    
-    resultElementSubtitle.style.color = 'red'
-    resultElementSubtitle.innerHTML = 'Ошибка! Заполните все обязательные поля!'   
+function renderEror() {    
+    blockResult.innerHTML = ''   
+    blockResult.style.color = 'red'
+    blockResult.innerHTML = 'Заполните все обязательные поля!'   
 }
 
 function renderInput() {
@@ -161,6 +149,7 @@ function renderInput() {
     input2.style.border = '1px solid #b9c0c5'
     input3.style.border = '1px solid #b9c0c5'
     input4.style.border = '1px solid #b9c0c5'
+    input5.style.border = '1px solid #b9c0c5'
 }
 
 
